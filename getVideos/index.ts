06a10993948853a94a29e659be0afa192d2f470c
Joshua McCarthy <joshua.f.mccarthy@gmail.com>
2022-02-10 16:57:40 +0900

    Removed async as it was causing errores

--- getVideos/index.ts
+++ getVideos/index.ts
@@ -1,12 +1,13 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+const httpTrigger: AzureFunction = function (context: Context, req: HttpRequest): void {
     context.log('HTTP trigger function processed a request.');
     const responseMessage = context.bindings.inputDocument
 
     context.res = {
         body: responseMessage
     };
+    context.done();
 
 };
 
